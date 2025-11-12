621bb0be39efac4c61c2158a81be8dc875c7194e
PeacefulKim <corbata@naver.com>
2025-11-12 15:41:18 +0900

    feat:

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Interaction : MonoBehaviour
 {
@@ -17,12 +18,49 @@ public class Interaction : MonoBehaviour
     private Camera _camera;
     void Start()
     {
-        
+        _camera = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time - lastCheckTime > checkRate)
+        {
+            lastCheckTime = Time.time;
+
+            Ray ray = _camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, maxCheckDistance, layerMask))
+            {
+                if (hit.collider.gameObject != currentInteractObj)
+                {
+                    currentInteractObj = hit.collider.gameObject;
+                    currentInteractable = hit.collider.GetComponent<IInteractable>();
+                    SetPromptText();
+                }
+            }
+            else
+            {
+                currentInteractObj = null;
+                currentInteractable = null;
+                promptText.gameObject.SetActive(false);
+            }
+        }
+    }
+    private void SetPromptText()
+    {
+        promptText.gameObject.SetActive(true);
+        promptText.text = currentInteractable.GetInteractPrompt();
+    }
+    public void OnInteractInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && currentInteractable != null)
+        {
+            currentInteractable.OnInteract();
+            currentInteractObj = null;
+            currentInteractable = null;
+            promptText.gameObject.SetActive(false);
+        }
     }
 }
+
